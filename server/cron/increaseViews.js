38a100678a03ef4160ae95dac9793d5c801f3db4
judeUzo <judeuzo7@gmail.com>
2025-12-16 18:06:31 +0100

    CRON JOB 5

--- server/cron/increaseViews.js
+++ server/cron/increaseViews.js
@@ -1,21 +1,14 @@
-import { autoIncreaseViews } from "../controllers/postController.js";
-import { connectDB } from "../utils/db.js";
+// server/cron/increaseViews.js
+import Post from "../models/post.js";
 
-export default async function handler(req, res) {
-  try {
-    // Allow GET or POST (easier for testing)
-    await connectDB();
-    await autoIncreaseViews();
+export const increaseViews = async () => {
+  const posts = await Post.find();
 
-    return res.status(200).json({
-      success: true,
-      message: "Views incremented successfully",
-    });
-  } catch (error) {
-    console.error("Increase views cron error:", error);
-    return res.status(500).json({
-      success: false,
-      message: "Failed to increase views",
-    });
+  for (const post of posts) {
+    const randomIncrement = Math.floor(Math.random() * 9) + 1;
+    post.views += randomIncrement;
+    await post.save();
   }
-}
+
+  console.log("✅ Views increased");
+};
